3945f97812819d5ac0e39cb5784bc6b0ce575da8
Noodlesoup29 <nathanlor2007@gmail.com>
2026-05-10 18:45:55 -0700

    Removed Song's API

--- FrontEnd/travel-agency-api/app/api/v1/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints.rapidapi import router as rapidapi_router
-from app.api.v1.endpoints.booking import router as booking_router
-
-
-api_router = APIRouter()
-api_router.include_router(rapidapi_router)
-api_router.include_router(booking_router)
--- FrontEnd/travel-agency-api/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# In app/models/__init__.py
-from .booking import User, Booking, HotelReservation, FlightReservation
-from .travel import Coordinates
